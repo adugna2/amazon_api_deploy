104c661044cad4ca8bbcb57d447109243bb229e1
adugna <adugnabezabih28@gmail.com>
2024-07-26 15:51:17 +0300

    edit

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable no-undef */
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+
+// // Load environment variables from .env file
+// dotenv.config();
+
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+// const app = express();
+// app.use(cors({ origin: true }));
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "success" });
+// });
+
+// // app.post("/payments/create", async (req, res) => {
+// //   const total = req.query.total;
+// //   if (total > 0) {
+// //     const paymentIntent = await stripe.paymentIntents.create({
+// //       amount: total,
+// //       currency: "usd"
+// //     });
+// app.post("/payment/create",async(req,res)=>{
+//   const total= parseInt(req.query.total);
+//   if (total > 0){
+//     const paymentIntent=await stripe.paymentIntents.create({
+//       amount:total,
+//       currency:"usd",
+//     })
+//     res.status(201).json({
+//       clientSecret: paymentIntent.client_secret,
+//     });
+//   } else {
+//     res.status(403).json({
+//       message: "total must be greater than 0",
+//     });
+//   }
+// });
+
+// const port = process.env.PORT || 5001;
+
+// app.listen(port, () => {
+//   console.log(`Amazon Server running on PORT: ${5001}, http://localhost:${5001}`);
+// });
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -17,20 +63,13 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "success" });
 });
 
-// app.post("/payments/create", async (req, res) => {
-//   const total = req.query.total;
-//   if (total > 0) {
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: total,
-//       currency: "usd"
-//     });
-app.post("/payment/create",async(req,res)=>{
-  const total= parseInt(req.query.total);
-  if (total > 0){
-    const paymentIntent=await stripe.paymentIntents.create({
-      amount:total,
-      currency:"usd",
-    })
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
     res.status(201).json({
       clientSecret: paymentIntent.client_secret,
     });
@@ -41,8 +80,7 @@ app.post("/payment/create",async(req,res)=>{
   }
 });
 
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-  console.log(`Amazon Server running on PORT: ${5001}, http://localhost:${5001}`);
+app.listen(5001, (err) => {
+  if (err) throw err;
+  console.log("Amazon API listening on port: 5001, http://localhost:5001) ");
 });
